9d09c138e8bbebc8a4eddf85a55ce2ff1f54ab28
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2021-07-24 21:05:29 -0300

    Actualizacion de comments

--- Suma/src/suma/SumaMain.java
+++ Suma/src/suma/SumaMain.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class SumaMain {
 
     public static void main(String[] args) {
-
+        
+        //Colocamos un scanner para ingresar datos por teclado
         Scanner entrada = new Scanner(System.in);
 
+        //Pedimos el primer valor al usuario
         System.out.println("Dame el primer valor: ");
         int valorUno = entrada.nextInt();
 
+        //Pedimos segundo valor al usuario
         System.out.println("Dame el segundo valor: ");
         int valorDos = entrada.nextInt();
 
+        //Reaizamos suma
         Suma valores = new Suma(valorUno, valorDos);
         valores.Imprimir();
     }
