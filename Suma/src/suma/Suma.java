041fb4655087e4634db32d6be690fd6651b87d07
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2021-07-24 21:03:23 -0300

    Add files via upload

---
+++ Suma/src/suma/Suma.java
@@ -0,0 +1,21 @@
+package suma;
+
+public class Suma {
+
+    private int vUno, vDos, Resultado;
+
+    public Suma(int valorUno, int valorDos) {
+        this.vUno = valorUno;
+        this.vDos = valorDos;
+    }
+
+    public void Operacion() {
+        Resultado = vUno + vDos;
+
+    }
+
+    public void Imprimir(){
+        Operacion();
+        System.out.println("El resultado de la suma es: " + Resultado);
+    }
+}
