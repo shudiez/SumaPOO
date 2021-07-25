bcec6fb18271b09a2e7ebb5bc90a28a003d576e1
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2021-07-24 21:07:30 -0300

    Actualizacion de comments

--- Suma/src/suma/Suma.java
+++ Suma/src/suma/Suma.java
@@ -1,17 +1,19 @@
 package suma;
 
 public class Suma {
-
+    
+    //Creamos variables
     private int vUno, vDos, Resultado;
 
+    //Constructores 
     public Suma(int valorUno, int valorDos) {
         this.vUno = valorUno;
         this.vDos = valorDos;
     }
-
+    
+    //Metodos
     public void Operacion() {
         Resultado = vUno + vDos;
-
     }
 
     public void Imprimir(){
